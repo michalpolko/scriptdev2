486dbfefc5d408071faabc38a0fe41d48818a45d
michalpolko <michalpolko@o2.pl>
2012-02-07 23:11:09 +0100

    [icc004] Some data to the enum and storing guids for some important creatures and gameobjects (OnCreature/GameObjectCreate).

--- scripts/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.h
+++ scripts/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.h
@@ -7,6 +7,7 @@
 
 enum
 {
+    // encounter types
     TYPE_MARROWGAR                  = 0,
     TYPE_LADY_DEATHWHISPER          = 1,
     TYPE_GUNSHIP_BATTLE             = 2,
@@ -19,7 +20,117 @@ enum
     TYPE_VALITHRIA                  = 9,
     TYPE_SINDRAGOSA                 = 10,
     TYPE_LICH_KING                  = 11,
-    MAX_ENCOUNTER                   = 12
+    MAX_ENCOUNTER                   = 12,
+
+    // other types, encounter helpers
+    TYPE_FROSTMOURNE_ROOM           = 13,
+
+    // NPC entries
+    // bosses
+    NPC_LORD_MARROWGAR          = 36612,
+    NPC_LADY_DEATHWHISPER       = 36855,
+    NPC_DEATHBRINGER_SAURFANG   = 37813,
+    NPC_FESTERGUT               = 36626,
+    NPC_ROTFACE                 = 36627,
+    NPC_PROFESSOR_PUTRICIDE     = 36678,
+    NPC_TALDARAM                = 37973,
+    NPC_VALANAR                 = 37970,
+    NPC_KELESETH                = 37972,
+    NPC_QUEEN_LANATHEL          = 37955,
+    NPC_VALITHRIA               = 36789,
+    NPC_SINDRAGOSA              = 36853,
+    NPC_LICH_KING               = 36597,
+
+    // boss-related and other NPCs
+    NPC_OVERLORD_SAURFANG       = 37187,
+    NPC_KORKRON_REAVER          = 37920,
+    NPC_MURADIN_BRONZEBEARD     = 37200, // Saurfang's encounter
+    NPC_ALLIANCE_MASON          = 37902,
+    NPC_BLOOD_ORB_CONTROL       = 38008,
+    NPC_LANATHEL_INTRO          = 38004,
+    NPC_VALITHRIA_QUEST         = 38589,
+    NPC_VALITHRIA_COMBAT_TRIGGER= 38752,
+    NPC_MURADIN                 = 36948, // Gunship Battle's encounter(?)
+    NPC_TIRION                  = 38995,
+    NPC_MENETHIL                = 38579,
+    NPC_FROSTMOURNE_TRIGGER     = 38584,
+    NPC_FROSTMOURNE_HOLDER      = 27880,
+    NPC_STINKY                  = 37025,
+    NPC_PRECIOUS                = 37217,
+    NPC_RIMEFANG                = 37533,
+    NPC_SPINESTALKER            = 37534,
+
+    // GameObjects entries
+    GO_ICEWALL_1                = 201911,
+    GO_ICEWALL_2                = 201910,
+    GO_MARROWGAR_DOOR           = 201857,
+
+    GO_ORATORY_DOOR             = 201563,
+    GO_DEATHWHISPER_ELEVATOR    = 202220,
+
+    GO_SAURFANG_DOOR            = 201825,
+
+    GO_GAS_RELEASE_VALVE        = 201616,
+    GO_ORANGE_PLAGUE            = 201371,
+    GO_GREEN_PLAGUE             = 201370,
+    GO_SCIENTIST_DOOR_GREEN     = 201614,
+    GO_SCIENTIST_DOOR_ORANGE    = 201613,
+    GO_SCIENTIST_DOOR_COLLISION = 201612,
+    GO_SCIENTIST_DOOR           = 201372,
+
+    GO_BLOODWING_DOOR           = 201920,
+    GO_CRIMSON_HALL_DOOR        = 201376,
+    GO_COUNCIL_DOOR_1           = 201377,
+    GO_COUNCIL_DOOR_2           = 201378,
+    GO_BLOODPRINCE_DOOR         = 201746,
+
+    GO_ICECROWN_GRATE           = 201755,
+    GO_FROSTWING_DOOR           = 201919,
+    GO_GREEN_DRAGON_DOOR_1      = 201375,
+    GO_GREEN_DRAGON_DOOR_2      = 201374,
+    GO_VALITHRIA_DOOR_1         = 201381,
+    GO_VALITHRIA_DOOR_2         = 201382,
+    GO_VALITHRIA_DOOR_3         = 201383,
+    GO_VALITHRIA_DOOR_4         = 201380,
+    GO_SINDRAGOSA_DOOR_1        = 201369,
+    GO_SINDRAGOSA_DOOR_2        = 201379,
+    GO_SINDRAGOSA_ENTRANCE      = 201373,
+
+    GO_FROZENTRONE_TR           = 202223,
+    GO_ICESHARD_1               = 202142,
+    GO_ICESHARD_2               = 202141,
+    GO_ICESHARD_3               = 202143,
+    GO_ICESHARD_4               = 202144,
+    GO_FROSTY_WIND              = 202188,
+    GO_FROSTY_EDGE              = 202189,
+    GO_SNOW_EDGE                = 202190,
+    GO_ARTHAS_PLATFORM          = 202161,
+    GO_ARTHAS_PRECIPICE         = 202078,
+
+    GO_PLAGUE_SIGIL             = 202182,
+    GO_FROSTWING_SIGIL          = 202181,
+    GO_BLOODWING_SIGIL          = 202183,
+
+    // loot chests
+    GO_SAURFANG_CACHE_10        = 202239,
+    GO_SAURFANG_CACHE_25        = 202240,
+    GO_SAURFANG_CACHE_10_H      = 202238,
+    GO_SAURFANG_CACHE_25_H      = 202241,
+
+    GO_GUNSHIP_ARMORY_A_10      = 201872,
+    GO_GUNSHIP_ARMORY_A_25      = 201873,
+    GO_GUNSHIP_ARMORY_A_10H     = 201874,
+    GO_GUNSHIP_ARMORY_A_25H     = 201875,
+
+    GO_GUNSHIP_ARMORY_H_10      = 202177,
+    GO_GUNSHIP_ARMORY_H_25      = 202178,
+    GO_GUNSHIP_ARMORY_H_10H     = 202179,
+    GO_GUNSHIP_ARMORY_H_25H     = 202180,
+
+    GO_DREAMWALKER_CACHE_10      = 201959,
+    GO_DREAMWALKER_CACHE_25      = 202339,
+    GO_DREAMWALKER_CACHE_10_H    = 202338,
+    GO_DREAMWALKER_CACHE_25_H    = 202340
 };
 
 class MANGOS_DLL_DECL instance_icecrown_citadel : public ScriptedInstance
@@ -49,6 +160,12 @@ class MANGOS_DLL_DECL instance_icecrown_citadel : public ScriptedInstance
     private:
         std::string m_strInstData;
         uint32 m_auiEncounter[MAX_ENCOUNTER];
+
+        // loot chests entries dependent on instance difficulty
+        uint32 m_uiGunshipArmoryA_ID;
+        uint32 m_uiGunshipArmoryH_ID;
+        uint32 m_uiSaurfangCache;
+        uint32 m_uiValithriaCache;
 };
 
 #endif
--- scripts/northrend/icecrown_citadel/icecrown_citadel/instance_icecrown_citadel.cpp
+++ scripts/northrend/icecrown_citadel/icecrown_citadel/instance_icecrown_citadel.cpp
@@ -32,6 +32,37 @@ instance_icecrown_citadel::instance_icecrown_citadel(Map *pMap) : ScriptedInstan
 void instance_icecrown_citadel::Initialize()
 {
     memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+
+    // to make respawning chests a little easier
+    switch (instance->GetDifficulty())
+    {
+        case RAID_DIFFICULTY_10MAN_NORMAL:
+            m_uiGunshipArmoryH_ID = GO_GUNSHIP_ARMORY_H_10;
+            m_uiGunshipArmoryA_ID = GO_GUNSHIP_ARMORY_A_10;
+            m_uiValithriaCache = GO_DREAMWALKER_CACHE_10;
+            m_uiSaurfangCache = GO_SAURFANG_CACHE_10;
+            break;
+        case RAID_DIFFICULTY_10MAN_HEROIC:
+            m_uiGunshipArmoryH_ID = GO_GUNSHIP_ARMORY_H_10H;
+            m_uiGunshipArmoryA_ID = GO_GUNSHIP_ARMORY_A_10H;
+            m_uiValithriaCache = GO_DREAMWALKER_CACHE_10_H;
+            m_uiSaurfangCache = GO_SAURFANG_CACHE_10_H;
+            break;
+        case RAID_DIFFICULTY_25MAN_NORMAL:
+            m_uiGunshipArmoryH_ID = GO_GUNSHIP_ARMORY_H_25;
+            m_uiGunshipArmoryA_ID = GO_GUNSHIP_ARMORY_A_25;
+            m_uiValithriaCache = GO_DREAMWALKER_CACHE_25;
+            m_uiSaurfangCache = GO_SAURFANG_CACHE_25;
+            break;
+        case RAID_DIFFICULTY_25MAN_HEROIC:
+            m_uiGunshipArmoryH_ID = GO_GUNSHIP_ARMORY_H_25H;
+            m_uiGunshipArmoryA_ID = GO_GUNSHIP_ARMORY_A_25H;
+            m_uiValithriaCache = GO_DREAMWALKER_CACHE_25_H;
+            m_uiSaurfangCache = GO_SAURFANG_CACHE_25_H;
+            break;
+        default:
+            break;
+    }
 }
 
 void instance_icecrown_citadel::Load(const char *strIn)
@@ -71,10 +102,155 @@ bool instance_icecrown_citadel::IsEncounterInProgress() const
 
 void instance_icecrown_citadel::OnObjectCreate(GameObject *pGo)
 {
+    switch(pGo->GetEntry())
+    {
+        case GO_ICEWALL_1:
+            if (m_auiEncounter[TYPE_MARROWGAR] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_ICEWALL_2:
+            if (m_auiEncounter[TYPE_MARROWGAR] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_DEATHWHISPER_ELEVATOR:
+        case GO_SAURFANG_DOOR:
+            if (m_auiEncounter[TYPE_DEATHBRINGER_SAURFANG] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_ORANGE_PLAGUE:
+            if (m_auiEncounter[TYPE_FESTERGUT] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_GREEN_PLAGUE:
+            if (m_auiEncounter[TYPE_ROTFACE] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_SCIENTIST_DOOR_GREEN:
+            if (m_auiEncounter[TYPE_ROTFACE] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_SCIENTIST_DOOR_ORANGE:
+            if (m_auiEncounter[TYPE_FESTERGUT] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_SCIENTIST_DOOR_COLLISION:
+            if (m_auiEncounter[TYPE_FESTERGUT] == DONE && m_auiEncounter[TYPE_ROTFACE] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_SCIENTIST_DOOR:
+            if (m_auiEncounter[TYPE_FESTERGUT] == DONE && m_auiEncounter[TYPE_ROTFACE] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_CRIMSON_HALL_DOOR:
+            if (m_auiEncounter[TYPE_BLOOD_PRINCE_COUNCIL] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_COUNCIL_DOOR_1:
+            if (m_auiEncounter[TYPE_BLOOD_PRINCE_COUNCIL] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_COUNCIL_DOOR_2:
+            if (m_auiEncounter[TYPE_BLOOD_PRINCE_COUNCIL] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_GREEN_DRAGON_DOOR_2:
+            if (m_auiEncounter[TYPE_VALITHRIA] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_SINDRAGOSA_DOOR_1:
+            if (m_auiEncounter[TYPE_VALITHRIA] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_SINDRAGOSA_DOOR_2:
+            if (m_auiEncounter[TYPE_VALITHRIA] == DONE)
+                pGo->SetGoState(GO_STATE_ACTIVE);
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+        case GO_SAURFANG_CACHE_10:
+        case GO_SAURFANG_CACHE_25:
+        case GO_SAURFANG_CACHE_10_H:
+        case GO_SAURFANG_CACHE_25_H:
+        case GO_GUNSHIP_ARMORY_A_10:
+        case GO_GUNSHIP_ARMORY_A_25:
+        case GO_GUNSHIP_ARMORY_A_10H:
+        case GO_GUNSHIP_ARMORY_A_25H:
+        case GO_GUNSHIP_ARMORY_H_10:
+        case GO_GUNSHIP_ARMORY_H_25:
+        case GO_GUNSHIP_ARMORY_H_10H:
+        case GO_GUNSHIP_ARMORY_H_25H:
+        case GO_DREAMWALKER_CACHE_10:
+        case GO_DREAMWALKER_CACHE_25:
+        case GO_DREAMWALKER_CACHE_10_H:
+        case GO_DREAMWALKER_CACHE_25_H:
+        case GO_ICESHARD_1:
+        case GO_ICESHARD_2:
+        case GO_ICESHARD_3:
+        case GO_ICESHARD_4:
+        case GO_FROSTY_WIND:
+        case GO_FROSTY_EDGE:
+        case GO_SNOW_EDGE:
+        case GO_ARTHAS_PLATFORM:
+        case GO_ARTHAS_PRECIPICE:
+        case GO_GAS_RELEASE_VALVE:
+        case GO_MARROWGAR_DOOR:
+        case GO_BLOODPRINCE_DOOR:
+        case GO_ICECROWN_GRATE:
+        case GO_SINDRAGOSA_ENTRANCE:
+        case GO_VALITHRIA_DOOR_1:
+        case GO_VALITHRIA_DOOR_2:
+        case GO_VALITHRIA_DOOR_3:
+        case GO_VALITHRIA_DOOR_4:
+        case GO_FROSTWING_DOOR:
+        case GO_GREEN_DRAGON_DOOR_1:
+        case GO_BLOODWING_DOOR:
+        case GO_ORATORY_DOOR:
+            m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
+            break;
+    }
 }
 
 void instance_icecrown_citadel::OnCreatureCreate(Creature *pCreature)
 {
+    switch(pCreature->GetEntry())
+    {
+        case NPC_LORD_MARROWGAR:
+        case NPC_LADY_DEATHWHISPER:
+        case NPC_DEATHBRINGER_SAURFANG:
+        case NPC_FESTERGUT:
+        case NPC_ROTFACE:
+        case NPC_PROFESSOR_PUTRICIDE:
+        case NPC_TALDARAM:
+        case NPC_VALANAR:
+        case NPC_KELESETH:
+        case NPC_QUEEN_LANATHEL:
+        case NPC_LANATHEL_INTRO:
+        case NPC_VALITHRIA:
+        case NPC_SINDRAGOSA:
+        case NPC_LICH_KING:
+        case NPC_TIRION:
+        case NPC_RIMEFANG:
+        case NPC_SPINESTALKER:
+        case NPC_STINKY:
+        case NPC_PRECIOUS:
+        case NPC_VALITHRIA_COMBAT_TRIGGER:
+        case NPC_BLOOD_ORB_CONTROL:
+             m_mNpcEntryGuidStore[pCreature->GetEntry()] = pCreature->GetObjectGuid();
+             break;
+    }
 }
 
 uint32 instance_icecrown_citadel::GetData(uint32 uiType)
