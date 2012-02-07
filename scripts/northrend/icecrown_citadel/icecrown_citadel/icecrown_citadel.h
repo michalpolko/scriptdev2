2ed85ecca3ec60fcd7ade16d7a8d3b45debb14ba
michalpolko <michalpolko@o2.pl>
2012-02-07 21:48:02 +0100

    [icc003] Base implementation for most important instance methods and encounter types added to the enum.

--- scripts/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.h
+++ scripts/northrend/icecrown_citadel/icecrown_citadel/icecrown_citadel.h
@@ -5,30 +5,50 @@
 #ifndef DEF_ICECROWN_CITADEL_H
 #define DEF_ICECROWN_CITADEL_H
 
-class MANGOS_DLL_DECL instance_icecrown_citadel : ScriptedInstance
+enum
+{
+    TYPE_MARROWGAR                  = 0,
+    TYPE_LADY_DEATHWHISPER          = 1,
+    TYPE_GUNSHIP_BATTLE             = 2,
+    TYPE_DEATHBRINGER_SAURFANG      = 3,
+    TYPE_FESTERGUT                  = 4,
+    TYPE_ROTFACE                    = 5,
+    TYPE_PROFESSOR_PUTRICIDE        = 6,
+    TYPE_BLOOD_PRINCE_COUNCIL       = 7,
+    TYPE_QUEEN_LANATHEL             = 8,
+    TYPE_VALITHRIA                  = 9,
+    TYPE_SINDRAGOSA                 = 10,
+    TYPE_LICH_KING                  = 11,
+    MAX_ENCOUNTER                   = 12
+};
+
+class MANGOS_DLL_DECL instance_icecrown_citadel : public ScriptedInstance
 {
     public:
         instance_icecrown_citadel(Map *pMap);
 
-        void Initialize() {}
-        void Load(const char* data);
-        const char* Save();
+        void Initialize();
+        void Load(const char* strIn);
 
-        void Update(uint32 uiDiff);
+        const char* Save()
+        {
+            return m_strInstData.c_str();
+        }
+
         bool IsEncounterInProgress() const;
 
-        void OnPlayerEnter(Player *pPlayer);
-        void OnPlayerDeath(Player *pPlayer);
-        void OnPlayerLeave(Player *pPlayer);
         void OnObjectCreate(GameObject *pGo);
         void OnCreatureCreate(Creature * pCreature);
-        void OnCreatureEnterCombat(Creature * pCreature);
-        void OnCreatureEvade(Creature *pCreature);
-        void OnCreatureDeath(Creature *pCreature);
 
         uint32 GetData(uint32 uiType);
         void SetData(uint32 uiType, uint32 uiData);
 
         bool CheckAchievementCriteriaMeet(uint32 uiCriteriaId, Player const* pSource, Unit const* pTarget = NULL, uint32 uiMiscvalue1 = 0);
         bool CheckConditionCriteriaMeet(Player const* pSource, uint32 uiMapId, uint32 uiInstanceConditionId);
+
+    private:
+        std::string m_strInstData;
+        uint32 m_auiEncounter[MAX_ENCOUNTER];
 };
+
+#endif
--- scripts/northrend/icecrown_citadel/icecrown_citadel/instance_icecrown_citadel.cpp
+++ scripts/northrend/icecrown_citadel/icecrown_citadel/instance_icecrown_citadel.cpp
@@ -16,13 +16,117 @@
 
 /* ScriptData
 SDName: instance_icecrown_citadel
-SD%Complete: 0
-SDComment: Placeholder
+SD%Complete:
+SDComment:
 SDCategory: Icecrown Citadel
 EndScriptData */
 
 #include "precompiled.h"
+#include "icecrown_citadel.h"
+
+instance_icecrown_citadel::instance_icecrown_citadel(Map *pMap) : ScriptedInstance(pMap)
+{
+    Initialize();
+}
+
+void instance_icecrown_citadel::Initialize()
+{
+    memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+}
+
+void instance_icecrown_citadel::Load(const char *strIn)
+{
+    if (!strIn)
+    {
+        OUT_LOAD_INST_DATA_FAIL;
+        return;
+    }
+
+    OUT_LOAD_INST_DATA(strIn);
+
+    std::istringstream loadStream(strIn);
+    loadStream >> m_auiEncounter[0] >> m_auiEncounter[1] >> m_auiEncounter[2] >> m_auiEncounter[3]
+        >> m_auiEncounter[4] >> m_auiEncounter[5] >> m_auiEncounter[6] >> m_auiEncounter[7] >> m_auiEncounter[8]
+        >> m_auiEncounter[9] >> m_auiEncounter[10] >> m_auiEncounter[11] >> m_auiEncounter[12] >> m_auiEncounter[13];
+
+    for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+    {
+        if (m_auiEncounter[i] == IN_PROGRESS)
+            m_auiEncounter[i] = NOT_STARTED;
+    }
+
+    OUT_LOAD_INST_DATA_COMPLETE;
+}
+
+bool instance_icecrown_citadel::IsEncounterInProgress() const
+{
+    for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+    {
+        if (m_auiEncounter[i] == IN_PROGRESS)
+            return true;
+    }
+
+    return false;
+}
+
+void instance_icecrown_citadel::OnObjectCreate(GameObject *pGo)
+{
+}
+
+void instance_icecrown_citadel::OnCreatureCreate(Creature *pCreature)
+{
+}
+
+uint32 instance_icecrown_citadel::GetData(uint32 uiType)
+{
+    if (uiType >= MAX_ENCOUNTER)
+        return 0;
+    else
+        return m_auiEncounter[uiType];
+}
+
+void instance_icecrown_citadel::SetData(uint32 uiType, uint32 uiData)
+{
+    if (uiData == DONE || uiData == FAIL)
+    {
+        OUT_SAVE_INST_DATA;
+
+        // Save all encounters, hard bosses, keepers and teleporters
+        std::ostringstream saveStream;
+        saveStream << m_auiEncounter[0] << " " << m_auiEncounter[1] << " " << m_auiEncounter[2] << " "
+            << m_auiEncounter[3] << " " << m_auiEncounter[4] << " " << m_auiEncounter[5] << " "
+            << m_auiEncounter[6] << " " << m_auiEncounter[7] << " " << m_auiEncounter[8] << " "
+            << m_auiEncounter[9] << " " << m_auiEncounter[10] << " " << m_auiEncounter[11] << " "
+            << m_auiEncounter[12] << " " << m_auiEncounter[13];
+
+        m_strInstData = saveStream.str();
+
+        SaveToDB();
+        OUT_SAVE_INST_DATA_COMPLETE;
+    }
+}
+
+bool instance_icecrown_citadel::CheckAchievementCriteriaMeet(uint32 uiCriteriaId, Player const* pSource, Unit const* pTarget, uint32 uiMiscvalue1)
+{
+    return false;
+}
+
+bool instance_icecrown_citadel::CheckConditionCriteriaMeet(Player const* pSource, uint32 uiMapId, uint32 uiInstanceConditionId)
+{
+    return false;
+}
+
+InstanceData* GetInstanceData_instance_icecrown_citadel(Map* pMap)
+{
+    return new instance_icecrown_citadel(pMap);
+}
 
 void AddSC_instance_icecrown_citadel()
 {
+    Script* pNewScript;
+
+    pNewScript = new Script;
+    pNewScript->Name = "instance_icecrown_citadel";
+    pNewScript->GetInstanceData = &GetInstanceData_instance_icecrown_citadel;
+    pNewScript->RegisterSelf();
 }
